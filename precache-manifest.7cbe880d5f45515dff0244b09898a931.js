723a80e3ded150db66681fe44732055107271795
Veniamin Tsigourof <tsigourof_ben@hotmail.com>
2019-09-23 01:47:03 +0300

    Updates

---
+++ precache-manifest.7cbe880d5f45515dff0244b09898a931.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4eab46fbeffbe6bb64b2747a77eab60",
+    "url": "/Flickr-GeoExplorer/index.html"
+  },
+  {
+    "revision": "bcecdce81c15d8bfd41d",
+    "url": "/Flickr-GeoExplorer/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "95c10ebb48aea76b8fa3",
+    "url": "/Flickr-GeoExplorer/static/js/2.428c4d4a.chunk.js"
+  },
+  {
+    "revision": "bcecdce81c15d8bfd41d",
+    "url": "/Flickr-GeoExplorer/static/js/main.b7899e3f.chunk.js"
+  },
+  {
+    "revision": "606dc6d3152a573c853a",
+    "url": "/Flickr-GeoExplorer/static/js/runtime~main.8e663d21.js"
+  },
+  {
+    "revision": "e88b541b221adb72de9c836a04b5af65",
+    "url": "/Flickr-GeoExplorer/static/media/PhotoMarker_20.e88b541b.svg"
+  }
+]);
